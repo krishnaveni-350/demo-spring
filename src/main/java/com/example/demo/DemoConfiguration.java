e1be819cff49cb681d1932ff6ff369dff3274a6d
krishnaveni <Krishnaveni.Velayutham@ibm.com>
2022-10-11 13:26:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoConfiguration {
+
+
+	@RequestMapping("/demo")
+	public List<Demo> retrieveAllCourses() {
+		return Arrays.asList(new Demo(1, "Learn AWS", "Pradeep"), new Demo(2, "Learn DevOps", "Pradeep"),
+				new Demo(3, "Learn Azure", "Pradeep"));
+	}
+
+}
